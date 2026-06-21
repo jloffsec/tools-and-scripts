53af860831e7044df6f41ef8e8f47174f854431b
Jordan López <32484424+jloffsec@users.noreply.github.com>
2026-06-21 14:58:21 +0200

    Add TTL-based OS detection via ping

--- enum/enum.sh
+++ enum/enum.sh
@@ -18,3 +18,15 @@ read TARGET
 echo ""
 echo "[*] Scanning $TARGET..."
 nmap -p- --open -sS -sC -sV --min-rate 5000 -n -vvv -Pn "$TARGET" -oN scan
+
+echo ""
+echo "[*] Checking connection:"
+TTL=$(ping -c 1 "$TARGET" | grep -oE "ttl=[0-9]+")
+
+if [ $TTL == "ttl=64" ]; then
+        echo "Linux host detected."
+elif [ $TTL == "ttl=128" ]; then
+        echo "Windows host detectted."
+else
+        echo "host type not detected."
+fi
